ec3bed463e59b727f789b39c8183dbb4e6dfe0ef
zzzoheir <zoheir.nejjari@ump.ac.ma>
2023-03-16 16:25:01 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints "Programming is like building a mltilingual
- * puzzle, followed by a newline."
+ * puzzle, followed by a new line."
  * return 0.
  */
 
@@ -13,5 +13,4 @@ int main(void)
 	puts("\"programming is like building a multilingual puzzle");
 
 	return(0);
-
 }
