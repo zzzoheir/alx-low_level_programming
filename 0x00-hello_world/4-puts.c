4f0535e837a49148a56b74d30c1f9f7160c3e224
zzzoheir <zoheir.nejjari@ump.ac.ma>
2023-03-16 16:21:16 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a mltilingual
+ * puzzle, followed by a newline."
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"programming is like building a multilingual puzzle");
+
+	return(0);
+
+}
