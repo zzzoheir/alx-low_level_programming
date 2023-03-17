cc6f15a2b5df3056c1eabd200bf67298beb16122
zzzoheir <zoheir.nejjari@ump.ac.ma>
2023-03-17 15:09:33 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,13 @@
 #include<stdlib.h>
 #include<time.h>
+/* more headers goes there */
 #include<stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	int n;
