9c5bd67d8016a4bd9c8b087689585a9d19af7432
zzzoheir <zoheir.nejjari@ump.ac.ma>
2023-03-16 17:01:13 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - this is the main function
- * 
+ *
  * Return: always 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
